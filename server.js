8be2f0dd000067c0be458114ed2fc8b8b7c062e6
Aboshady48 <aboshady48585@gmail.com>
2025-03-17 07:08:39 +0200

    add morgan logger

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from "dotenv"
+import morgan from 'morgan';
 
 dotenv.config({
     path: './config/.env'
@@ -9,6 +10,7 @@ const app = express()
 const port = 3000;
 
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.send('Welcome to Mern Notes App')
